54d1fdfa67112961dd85388845398f307fb29bde
Sangyoon Park <datumvitae@gmail.com>
2023-11-10 10:42:47 -0500

    Set up integration testing

---
+++ tests/integration/conftest.py
@@ -0,0 +1,10 @@
+import pytest
+
+from survey_processor import SurveyProcessor
+from tests.utils import instantiate_survey_processor
+
+
+@pytest.fixture
+def processor_for_holidays_survey() -> SurveyProcessor:
+    path_to_xlsform = "tests/data/stats4sd_example_xlsform_adapted.xlsx"
+    return instantiate_survey_processor(path_to_xlsform)
---
+++ tests/integration/survey_processor/test_navigation.py
@@ -0,0 +1,28 @@
+import pytest
+
+from survey_processor import SurveyProcessor
+
+
+@pytest.fixture
+def processor(processor_for_holidays_survey) -> SurveyProcessor:
+    return processor_for_holidays_survey
+
+
+def test_survey_start(processor: SurveyProcessor):
+    assert processor.curr_survey_start is True
+
+
+def test_survey_next(processor: SurveyProcessor):
+    processor.next()
+    assert processor.curr_name == "intro.note"
+    processor.next()
+    assert processor.curr_name == "name"
+    processor.next()
+    assert processor.curr_name == "date.birth"
+
+
+def test_survey_end(processor: SurveyProcessor):
+    processor.next()
+    processor._session.add_new_visit("holiday.activity.3")
+    processor.next()
+    assert processor.curr_survey_end is True
