a4dbf34da99002342095190a012ea32b3987518b
Sangyoon Park <datumvitae@gmail.com>
2024-01-04 20:13:39 -0600

    Create pages and forms for authentication

--- safely_report/auth/views.py
+++ safely_report/auth/views.py
@@ -1,9 +1,20 @@
-from flask import Blueprint, redirect, url_for
+from flask import Blueprint, redirect, render_template, url_for
 from flask_login import current_user, login_required, login_user, logout_user
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 from safely_report.models import Role, User
 
 auth_blueprint = Blueprint("auth", __name__, template_folder="templates/auth")
+
+
+def make_auth_form(label: str) -> FlaskForm:
+    class AuthForm(FlaskForm):
+        field = PasswordField(label, validators=[DataRequired()])
+        submit = SubmitField("Submit")
+
+    return AuthForm()
 
 
 @auth_blueprint.route("/")
@@ -11,16 +22,22 @@ def index():
     if current_user.is_authenticated:
         if current_user.role == Role.Respondent:
             return redirect(url_for("survey.index"))
-    return "Please log in"
+    return render_template("select_role.html")
 
 
-@auth_blueprint.route("/login/respondent/<uuid>")
-def login_respondent(uuid):
-    user = User.query.get(uuid)
-    if user is not None and user.role == Role.Respondent:
-        login_user(user)
-        return redirect(url_for("survey.index"))
-    return "Respondent not found"
+@auth_blueprint.route("/login/respondent", methods=["GET", "POST"])
+def login_respondent():
+    form = make_auth_form("Please enter your UUID:")
+
+    if form.validate_on_submit():
+        uuid = form.field.data
+        user = User.query.get(uuid)
+        if user is not None and user.role == Role.Respondent:
+            login_user(user)
+            return redirect(url_for("survey.index"))
+        return "Respondent not found"  # TODO: Replace with flash message
+
+    return render_template("authenticate.html", form=form)
 
 
 @auth_blueprint.route("/logout")
