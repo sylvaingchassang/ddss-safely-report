1bf8d3d0025527bb38cb4db6b8b975c0e3cbe79f
Sangyoon Park <datumvitae@gmail.com>
2024-01-09 13:14:54 -0600

    Set up admin interface

--- safely_report/__init__.py
+++ safely_report/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from flask_admin import Admin
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_session import Session
 
+from safely_report.admin.views import (
+    SurveyAdminIndexView,
+    SurveyAdminModelView,
+)
 from safely_report.auth.views import auth_blueprint
-from safely_report.models import User, db
+from safely_report.models import Enumerator, Respondent, User, db
 from safely_report.survey.views import survey_blueprint
 
 
@@ -27,6 +32,11 @@ def create_app() -> Flask:
     @login_manager.user_loader
     def load_user(user_uuid):
         return User.query.filter_by(uuid=user_uuid).first()
+
+    # Set up admin interface
+    admin = Admin(app, name="Safely Report", index_view=SurveyAdminIndexView())
+    admin.add_view(SurveyAdminModelView(Respondent, db.session))
+    admin.add_view(SurveyAdminModelView(Enumerator, db.session))
 
     # Register routes (i.e., "views")
     app.register_blueprint(auth_blueprint, url_prefix="/auth")
---
+++ safely_report/admin/views.py
@@ -0,0 +1,30 @@
+from flask_admin import AdminIndexView, expose
+from flask_admin.contrib.sqla import ModelView
+from flask_admin.form import SecureForm
+
+
+class SurveyAdminIndexView(AdminIndexView):
+    @expose("/")
+    def index(self):
+        return self.render("admin/index.html")
+
+
+class SurveyAdminModelView(ModelView):
+    form_base_class = SecureForm
+
+    # Display UUID
+    column_display_pk = True
+    column_labels = {"uuid": "UUID"}
+
+    # Show UUID in the final column
+    def scaffold_list_columns(self):
+        columns: list = super().scaffold_list_columns()
+        columns.remove("uuid")
+        columns.append("uuid")
+        return columns
+
+    # Disable sorting for UUID
+    def scaffold_sortable_columns(self):
+        columns: dict = super().scaffold_sortable_columns()
+        columns.pop("uuid")
+        return columns
