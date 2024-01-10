e6a548dcb47f54d79ff918ae7b0fc278a019ca3f
Sangyoon Park <datumvitae@gmail.com>
2024-01-10 15:21:03 -0600

    Require auth to access admin interface

--- safely_report/admin/views.py
+++ safely_report/admin/views.py
@@ -1,6 +1,9 @@
 from flask_admin import AdminIndexView, expose
 from flask_admin.contrib.sqla import ModelView
 from flask_admin.form import SecureForm
+from flask_login import current_user
+
+from safely_report.models import Role
 
 
 class SurveyAdminIndexView(AdminIndexView):
@@ -8,10 +11,22 @@ class SurveyAdminIndexView(AdminIndexView):
     def index(self):
         return self.render("admin/index.html")
 
+    def is_accessible(self):
+        return getattr(current_user, "role", None) == Role.Admin
+
+    def inaccessible_callback(self, name, **kwargs):
+        return "Unauthorized"  # TODO: Flash message and redirect
+
 
 class SurveyModelView(ModelView):
     form_base_class = SecureForm
     column_display_pk = True
+
+    def is_accessible(self):
+        return getattr(current_user, "role", None) == Role.Admin
+
+    def inaccessible_callback(self, name, **kwargs):
+        return "Unauthorized"  # TODO: Flash message and redirect
 
 
 class RespondentModelView(SurveyModelView):
