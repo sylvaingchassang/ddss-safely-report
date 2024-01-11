63232c4571131a19b8a53e8e53410a9eee32e71e
Sangyoon Park <datumvitae@gmail.com>
2024-01-11 10:56:55 -0600

    Set up enumerator view

--- safely_report/enumerator/views.py
+++ safely_report/enumerator/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 from flask_login import current_user
 
 from safely_report.models import Respondent
@@ -8,5 +8,16 @@ enum_blueprint = Blueprint("enum", __name__)
 
 @enum_blueprint.route("/")
 def index():
+    # List respondent attributes to display
+    attributes = Respondent.__table__.columns.keys()
+    attributes_to_hide = ["uuid", "enumerator_uuid", "id"]
+    attributes_to_show = [a for a in attributes if a not in attributes_to_hide]
+
+    # Retrieve assigned respondent records
     respondents = Respondent.query.filter_by(enumerator=current_user).all()
-    return [str(r) for r in respondents]
+
+    return render_template(
+        "enumerator/index.html",
+        attributes=attributes_to_show,
+        respondents=respondents,
+    )
