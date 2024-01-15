b245e0b590a4fdc14f72f3dba2989fe319a1b671
Sangyoon Park <datumvitae@gmail.com>
2024-01-15 11:58:04 -0600

    Update the way an enumerator takes survey on behalf of a respondent
    Rather than logging in and completing the survey under their own enumerator
    identity (with the respondent's identity cached as metadata), an enumerator
    will be re-logged in as the respondent that they are helping and be completing
    the survey under the respondent's identity directly (with their own enumerator
    identity cached as metadata).

--- safely_report/enumerator/views.py
+++ safely_report/enumerator/views.py
@@ -1,8 +1,15 @@
-from flask import Blueprint, render_template
-from flask_login import current_user, login_required
+from flask import (
+    Blueprint,
+    redirect,
+    render_template,
+    request,
+    session,
+    url_for,
+)
+from flask_login import current_user, login_required, login_user
 
 from safely_report.auth.utils import role_required
-from safely_report.models import Respondent, Role
+from safely_report.models import Respondent, Role, User
 
 enumerator_blueprint = Blueprint("enumerator", __name__)
 
@@ -14,8 +21,21 @@ def require_auth():
     pass  # Actual implementation handled by decorator
 
 
-@enumerator_blueprint.route("/")
+@enumerator_blueprint.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        respondent_uuid = request.form.get("respondent_uuid")
+        enumerator_uuid = current_user.uuid
+
+        # Access the survey on behalf of the respondent
+        user = User.query.filter_by(uuid=respondent_uuid).first()
+        if user is not None and user.role == Role.Respondent:
+            session.clear()
+            session["enumerator_uuid"] = enumerator_uuid
+            login_user(user)
+            return redirect(url_for("survey.index"))
+        return "Respondent not found"  # TODO: Flash message and redirect
+
     # List respondent attributes to display
     attributes = Respondent.__table__.columns.keys()
     attributes_to_hide = ["uuid", "enumerator_uuid", "id"]
--- safely_report/survey/views.py
+++ safely_report/survey/views.py
@@ -1,7 +1,6 @@
 from flask import Blueprint, redirect, render_template, session, url_for
 from flask_login import current_user, login_required
 
-from safely_report.auth.utils import role_required
 from safely_report.models import Role, db
 from safely_report.settings import XLSFORM_PATH
 from safely_report.survey.form_generator import SurveyFormGenerator
@@ -19,14 +18,6 @@ form_generator = SurveyFormGenerator(survey_processor)
 survey_blueprint = Blueprint("survey", __name__)
 
 
-def curr_testing_mode() -> bool:
-    if current_user.role == Role.Respondent:
-        return False
-    if current_user.role == Role.Enumerator and survey_session.respondent_uuid:
-        return False
-    return True
-
-
 @survey_blueprint.before_request
 @login_required
 def require_auth():
@@ -35,10 +26,12 @@ def require_auth():
 
 @survey_blueprint.route("/", methods=["GET", "POST"])
 def index():
+    curr_testing_mode = current_user.role != Role.Respondent
+
     if survey_processor.curr_survey_end:
         return render_template(
             "survey/submit.html",
-            curr_testing_mode=curr_testing_mode(),
+            curr_testing_mode=curr_testing_mode,
         )
 
     if survey_processor.curr_survey_start:
@@ -54,24 +47,8 @@ def index():
         "survey/index.html",
         form=form,
         survey_processor=survey_processor,
-        curr_testing_mode=curr_testing_mode(),
+        curr_testing_mode=curr_testing_mode,
     )
-
-
-@role_required(Role.Enumerator)
-@survey_blueprint.route("/on-behalf-of/<respondent_uuid>")
-def on_behalf_of(respondent_uuid: str):
-    """
-    Cache respondent UUID if enumerator conducts survey on their behalf.
-    """
-    if survey_session.respondent_uuid is None:
-        survey_session.set_respondent_uuid(respondent_uuid)
-        return redirect(url_for("survey.index"))
-    elif survey_session.respondent_uuid == respondent_uuid:
-        return redirect(url_for("survey.index"))
-    else:
-        # TODO: Flash message ("Survey in progress for another respondent")
-        return redirect(url_for("enumerator.index"))
 
 
 @survey_blueprint.route("/back")
@@ -90,23 +67,17 @@ def submit():
     if not survey_processor.curr_survey_end:
         return redirect(url_for("survey.index"))
 
-    if curr_testing_mode():
+    if current_user.role != Role.Respondent:
         return redirect(url_for("index"))  # TODO: Flash message
 
-    # Identify current respondent
-    if current_user.role == Role.Respondent:
-        respondent_uuid = current_user.uuid
-    elif current_user.role == Role.Enumerator:
-        respondent_uuid = survey_session.respondent_uuid
-    else:
-        # TODO: Flash message and redirect
-        return f"Unknown role: {current_user.role}"
-
     # Garble survey response and store it into database
-    survey_response = survey_processor.gather_survey_response()
-    garbler.garble_and_store(survey_response, respondent_uuid)
+    garbler.garble_and_store(
+        survey_response=survey_processor.gather_survey_response(),
+        respondent_uuid=current_user.uuid,
+        enumerator_uuid=session.get("enumerator_uuid"),
+    )
 
     # Clear session data if response has been successfully stored in database
-    survey_processor.clear_session()
+    session.clear()
 
     return "Survey response submitted"  # TODO: Flash message and redirect
