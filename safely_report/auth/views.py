fec3513fc8ebb105ca4b27f4e410b89ffde95aaa
Sangyoon Park <datumvitae@gmail.com>
2024-02-06 15:12:38 -0500

    Add logging to auth routes

--- safely_report/auth/views.py
+++ safely_report/auth/views.py
@@ -1,4 +1,11 @@
-from flask import Blueprint, redirect, render_template, session, url_for
+from flask import (
+    Blueprint,
+    current_app,
+    redirect,
+    render_template,
+    session,
+    url_for,
+)
 from flask_login import login_required, login_user, logout_user
 
 from safely_report.auth.utils import make_auth_form
@@ -22,7 +29,9 @@ def login_respondent():
         user = User.query.filter_by(uuid=uuid).first()
         if user is not None and user.role == Role.Respondent:
             login_user(user)
+            current_app.logger.info("Respondent login")
             return redirect(url_for("survey.index"))
+        current_app.logger.info("Respondent login failed")
         return "Respondent not found"  # TODO: Flash message and redirect
 
     return render_template("auth/submit.html", form=form)
@@ -37,7 +46,9 @@ def login_enumerator():
         user = User.query.filter_by(uuid=uuid).first()
         if user is not None and user.role == Role.Enumerator:
             login_user(user)
+            current_app.logger.info("Enumerator login")
             return redirect(url_for("enumerator.index"))
+        current_app.logger.info("Enumerator login failed")
         return "Enumerator not found"  # TODO: Flash message and redirect
 
     return render_template("auth/submit.html", form=form)
@@ -52,7 +63,9 @@ def login_admin():
         if password == ADMIN_PASSWORD:
             user = User.get_admin()
             login_user(user)
+            current_app.logger.info("Admin login")
             return redirect(url_for("admin.index"))
+        current_app.logger.info("Admin login failed")
         return "Invalid password"  # TODO: Flash message and redirect
 
     return render_template("auth/submit.html", form=form)
@@ -63,4 +76,5 @@ def login_admin():
 def logout():
     logout_user()
     session.clear()
+    current_app.logger.info("User logout")
     return redirect(url_for("auth.index"))
