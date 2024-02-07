7846dc7fc03b5414a389dfeb9055ca0eb17aeb92
Sangyoon Park <datumvitae@gmail.com>
2024-02-07 05:17:10 -0500

    Log more useful info
    Note that user IDs do not compromise privacy because
    they are generated and used for the application's
    internal operations only.
    
    In fact, user table itself is not accessible from
    the admin view, so this logging info is strictly for
    debugging purposes.

--- safely_report/auth/views.py
+++ safely_report/auth/views.py
@@ -6,7 +6,7 @@ from flask import (
     session,
     url_for,
 )
-from flask_login import login_required, login_user, logout_user
+from flask_login import current_user, login_required, login_user, logout_user
 
 from safely_report.auth.utils import make_auth_form
 from safely_report.models import Role, User
@@ -29,9 +29,9 @@ def login_respondent():
         user = User.query.filter_by(uuid=uuid).first()
         if user is not None and user.role == Role.Respondent:
             login_user(user)
-            current_app.logger.info("Respondent login")
+            current_app.logger.info(f"Login - user {user.id}")
             return redirect(url_for("survey.index"))
-        current_app.logger.info("Respondent login failed")
+        current_app.logger.info(f"Failed login - user {user.id}")
         return "Respondent not found"  # TODO: Flash message and redirect
 
     return render_template("auth/submit.html", form=form)
@@ -46,9 +46,9 @@ def login_enumerator():
         user = User.query.filter_by(uuid=uuid).first()
         if user is not None and user.role == Role.Enumerator:
             login_user(user)
-            current_app.logger.info("Enumerator login")
+            current_app.logger.info(f"Login - user {user.id}")
             return redirect(url_for("enumerator.index"))
-        current_app.logger.info("Enumerator login failed")
+        current_app.logger.info(f"Failed login - user {user.id}")
         return "Enumerator not found"  # TODO: Flash message and redirect
 
     return render_template("auth/submit.html", form=form)
@@ -63,9 +63,9 @@ def login_admin():
         if password == ADMIN_PASSWORD:
             user = User.get_admin()
             login_user(user)
-            current_app.logger.info("Admin login")
+            current_app.logger.info(f"Login - user {user.id}")
             return redirect(url_for("admin.index"))
-        current_app.logger.info("Admin login failed")
+        current_app.logger.info(f"Failed login - user {user.id}")
         return "Invalid password"  # TODO: Flash message and redirect
 
     return render_template("auth/submit.html", form=form)
@@ -74,7 +74,8 @@ def login_admin():
 @auth_blueprint.route("/logout")
 @login_required
 def logout():
+    id = current_user.id
     logout_user()
     session.clear()
-    current_app.logger.info("User logout")
+    current_app.logger.info(f"Logout - user {id}")
     return redirect(url_for("auth.index"))
