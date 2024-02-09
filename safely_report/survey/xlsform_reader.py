040aaf666e6a5eccf10140a883e910004991f4f0
Sangyoon Park <datumvitae@gmail.com>
2024-02-09 10:10:01 -0500

    Error out if XSLForm has infinite repeats

--- safely_report/survey/xlsform_reader.py
+++ safely_report/survey/xlsform_reader.py
@@ -1,11 +1,12 @@
 from pyxform import Survey
 from pyxform.builder import create_survey_element_from_dict
+from pyxform.survey_element import SurveyElement
 from pyxform.xls2json import parse_file_to_json
 
 
 class XLSFormReader:
-    @staticmethod
-    def read_xlsform(path_to_xlsform: str) -> Survey:
+    @classmethod
+    def read_xlsform(cls, path_to_xlsform: str) -> Survey:
         """
         Parse and validate XLSForm to generate its Python representation.
 
@@ -19,9 +20,27 @@ class XLSFormReader:
             path=path_to_xlsform,
             default_name="__survey__",
         )
-        survey_tree = create_survey_element_from_dict(survey_dict)
+        survey = create_survey_element_from_dict(survey_dict)
 
-        # TODO: Validate the form
-        assert isinstance(survey_tree, Survey)
+        # Ensure the form meets requirements specific to our application
+        assert isinstance(survey, Survey)
+        for element in survey.iter_descendants():
+            cls._check_infinite_repeat(element)
 
-        return survey_tree
+        return survey
+
+    @classmethod
+    def _check_infinite_repeat(cls, element: SurveyElement):
+        """
+        Error out if the given survey element is an infinite repeat.
+
+        The application does not support infinite repeats for now. Instead,
+        survey designers can use repeats where the respondent can dynamically
+        set and change the number of repeats. For instance, if the survey has
+        a repeat section asking details of family members (e.g., name, age),
+        it can first ask how many family members the respondent has and then,
+        based on this response, perform repeats.
+        """
+        if element.type == "repeat":
+            if element.control.get("jr:count") is None:
+                raise Exception(f"Infinite repeat not allowed: {element.name}")
