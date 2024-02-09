339d01a0e018088ad0e38019069c421fab4e6bf3
Sangyoon Park <datumvitae@gmail.com>
2024-02-09 10:44:50 -0500

    Error out if XSLForm has nested repeats

--- safely_report/survey/xlsform_reader.py
+++ safely_report/survey/xlsform_reader.py
@@ -26,6 +26,7 @@ class XLSFormReader:
         assert isinstance(survey, Survey)
         for element in survey.iter_descendants():
             cls._check_infinite_repeat(element)
+            cls._check_nested_repeat(element)
 
         return survey
 
@@ -44,3 +45,20 @@ class XLSFormReader:
         if element.type == "repeat":
             if element.control.get("jr:count") is None:
                 raise Exception(f"Infinite repeat not allowed: {element.name}")
+
+    @classmethod
+    def _check_nested_repeat(cls, element: SurveyElement):
+        """
+        Error out if the given survey element is a nested repeat.
+
+        The application does not support nested repeats (i.e., repeat section
+        within another repeat section); it only supports single-depth repeats.
+        We made this choice because nested repeats introduce unnecessary
+        complication to survey administration itself.
+        """
+        if element.type == "repeat":
+            for descendant in element.iter_descendants():
+                if descendant != element and descendant.type == "repeat":
+                    raise Exception(
+                        f"Nested repeat not allowed: {element.name}"
+                    )
