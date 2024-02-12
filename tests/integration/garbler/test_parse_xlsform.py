4bf0c212c8b447acb487eb74874da992ef77920b
Sangyoon Park <datumvitae@gmail.com>
2024-02-12 12:01:20 -0500

    Add test for checking garbling in repeat section

--- tests/integration/garbler/test_parse_xlsform.py
+++ tests/integration/garbler/test_parse_xlsform.py
@@ -1,3 +1,8 @@
+import tempfile
+
+import openpyxl
+import pytest
+
 from safely_report.survey.garbling import Garbler, GarblingScheme
 
 
@@ -8,3 +13,15 @@ def test_parse_xlsform(path_to_xlsform_holidays):
     assert params.get("like.travel").scheme == GarblingScheme.PopBlock
     assert params.get("ever.abroad")
     assert params.get("ever.abroad").scheme == GarblingScheme.IID
+
+
+def test_xlsform_validation(path_to_xlsform_test_cases):
+    with tempfile.NamedTemporaryFile(suffix=".xlsx") as temp_file:
+        workbook = openpyxl.load_workbook(path_to_xlsform_test_cases)
+        sheet = workbook["garbling_in_repeat"]
+        sheet.title = "survey"
+        workbook.save(temp_file.name)
+
+        with pytest.raises(Exception) as e:
+            Garbler._parse_xlsform(temp_file.name)
+        assert str(e.value) == "Garbling should not be applied inside repeats"
